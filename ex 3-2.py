002f05eb62837be4e8ecfb8d1d44022dbb173def
richpapa1104-creator <richpapa1104@gmail.com>
2025-11-03 20:54:06 +0800

    Create ex 3-2.py

---
+++ ex 3-2.py
@@ -0,0 +1,84 @@
+'''
+# 建立變數
+n2ame = "曹友謙"
+age_old = 13
+class1 = 175
+is_studemt = True
+
+# 建立變數
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+
+
+
+# 字串 (String)
+name = "20"
+
+print(name)
+print(type(name))      # <class 'str'>
+# 整數 (Integer)
+age = 25
+name = 20
+print(type(age))       # <class 'int'>
+print(name)
+print(type(name))      # <class 'int'>
+# 浮點數 (Float)
+height = 175.6
+
+print(type(height))    # <class 'float'>
+# 布林值 (Boolean)
+is_student = True
+print(type(is_student)) # <class 'bool'>
+
+
+# 整數 (Integer)
+age = input("please input int number = ")
+print("age = ",age)
+print(type(age))
+
+age = int(input("please input int number = "))
+print("age = ",age)
+print(type(age))
+
+
+print("hello")
+
+# 字串 (String)
+name = "20"
+
+print(name)
+print(type(name))      # <class 'str'>
+# 整數 (Integer)
+age = 25
+name = 20
+print(type(age))       # <class 'int'>
+print(name)
+print(type(name))      # <class 'int'>
+# 浮點數 (Float)
+height = 175.6
+
+print(type(height))    # <class 'float'>
+# 布林值 (Boolean)
+is_student = True
+print(type(is_student)) # <class 'bool'>
+
+
+# 整數 (Integer)
+age = input("please input int number = ")
+print("age = ",age)
+print(type(age))
+
+age = int(input("please input int number = "))
+print("age = ",age)
+print(type(age))
+# 浮點數 (Float)
+height = input("please input float number = ")
+print("height = ",height)
+print(type(height))
+
+height = float(input("please input float number = "))
+print("height = ",height)
+print(type(height))
