8372dc4b8517d56c2d5e06508b2af3701e216774
richpapa1104-creator <richpapa1104@gmail.com>
2025-11-17 20:47:17 +0800

    ex4-7.py

---
+++ ex4-7.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+  
+  
+age = int(input("please input a num: "))
+
+if age >= 18:
+    print("您已成年，可以投票！")
+else:
+    print("您未成年，無法投票。")
+print(age)
